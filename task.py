9ac13e347087b881bb03f805ea2b35f6b12a41e2
Sanjjushri <67419416+Sanjjushri@users.noreply.github.com>
2021-05-12 23:19:02 +0530

    Updated file - flask

--- task.py
+++ task.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///task.db"
+app.config['SECRET_KEY']= '6dbf57e6dd405d361aa7db16'
 db= SQLAlchemy(app)
 
 
 class User(db.Model):
     id= db.Column(db.Integer(), primary_key=True)
-    username= db.Column(db.String(lenght=30), nullable=False, unique=True)
+    username= db.Column(db.String(length=30), nullable=False, unique=True)
     email_address= db.Column(db.String(length=50), nullable=False, unique=True)
     password= db.Column(db.String(length=60), nullable=False)
     items = db.relationship("Item", backref='owned_user', lazy=True)
@@ -21,6 +25,15 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.task}'
+
+
+
+class SignUp(FlaskForm):
+    username = StringField(label='username')
+    email= StringField(lable='email')
+    password1= PasswordField(lable='password1')
+    passwoord2= PasswordField(lable='password2')
+    submit = SubmitField(lable='submit')
 
 @app.route('/')
 @app.route('/home')
@@ -39,7 +52,7 @@ def status_update():
 def login_page():
     return render_template("login.html")
 
-@app.route('/sign')
+@app.route('/signup')
 def sign_up():
     return render_template("sign.html")
 
